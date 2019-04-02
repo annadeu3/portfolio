0e37ef9947f4e05d9b15ddd01d37f93532d6d01c
Anna Deu <annadeu3@gmail.com>
2019-04-01 22:37:56 -0400

    google font change

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
-        typekit: {
+        google: {
           families: ['futura-pt']
         }
       }
