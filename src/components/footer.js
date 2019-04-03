042690e47954f591ada9d9edcb28ac480967d3ed
Anna Deu <annadeu3@gmail.com>
2019-04-02 20:46:02 -0400

    fix class to className in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,14 @@
 import React from "react"
 
 const Footer = () => (
-    <footer>
-        <div class="a-container">
-        <section id="mainfooter">
-            <h1 class="a-left">
-            For inquiries
-            </h1> 
-        </section>
-        {new Date().getFullYear()}
-        </div>
-    </footer>
+  <footer>
+    <div className="a-container">
+      <section id="mainfooter">
+        <h1 className="a-left">For inquiries</h1>
+      </section>
+      {new Date().getFullYear()}
+    </div>
+  </footer>
 )
 
-export default Footer;
+export default Footer
