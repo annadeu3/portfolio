934ffd8d77659c008450e2fb9dff9b634823ae64
Anna Deu <annadeu3@gmail.com>
2019-04-07 22:28:46 -0400

    footer social links added

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,8 +4,19 @@ const Footer = () => (
   <footer>
     <div className="a-container">
       <section id="mainfooter">
-        <h1 className="a-left">For inquiries</h1>
+        <h1 className="a-left">
+          For inquiries and collaboration, contact me here:
+          <br />
+          annadeu3@gmail.com
+        </h1>
       </section>
+      <div className="social">
+        <h5>Find me online</h5>
+        <p>LinkedIn</p>
+        <p>Twitter</p>
+        <p>Medium</p>
+        <p>Instagram</p>
+      </div>
       {new Date().getFullYear()}
     </div>
   </footer>
