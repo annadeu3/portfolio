df0e68bc0553ca06681a7b531f9863894f8e0de7
Anna Deu <annadeu3@gmail.com>
2019-04-21 17:39:15 -0400

    fullpage scroll for the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,11 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import poop from "../images/csl.png"
+import ReactFullpage from "@fullpage/react-fullpage"
+
+const pluginWrapper = () => {
+  require("fullpage.js/vendors/scrolloverflow")
+}
 
 class IndexPage extends React.Component {
   constructor(props) {
@@ -22,9 +27,6 @@ class IndexPage extends React.Component {
   typeWriter = (text, i, fnCallback) => {
     // check if text isn't finished yet
     if (i < text.length) {
-      // add next character to h1
-      console.log("text chage")
-
       this.topicRef.current.innerHTML =
         text.substring(0, i + 1) + '<span aria-hidden="true"></span>'
 
@@ -70,110 +72,130 @@ class IndexPage extends React.Component {
     return (
       <Layout>
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-        <h2>hello.</h2>
-        <h2>
-          <strong>UX Designer based in NYC—</strong>
-        </h2>
 
-        <h3>
-          Currently at Vertic, creating intentional and thoughtful experiences
-          for users.
-        </h3>
-        <h3>
-          I'm always happy to chat about
-          <br />
-          <span className="topic" ref={this.topicRef}>
-            User experience design
-          </span>
-        </h3>
-        <br />
-        <br />
-        <section id="project1" class="section">
-          <div class="project-container">
-            <div class="project-title">
-              <h1 class="big-title"> CSL</h1>
-              <div class="project-short-description">
-                <div class="dash"> </div>
-                <p>
-                  {" "}
-                  Led the UX Design of the website redesign of CSL and CSL
-                  Behring, a global specialty biotechnology company.
-                </p>
+        <ReactFullpage
+          render={({ state, fullpageApi }) => {
+            console.log("render prop change", state) // eslint-disable-line no-console
+
+            if (state.callback === "onLeave") {
+              if (state.direction === "down") {
+                console.log("going down..." + state.origin.index)
+              }
+            }
+            return (
+              <div id="fullpage-wrapper">
+                <div className="section section1">
+                  <h2>hello.</h2>
+                  <h2>
+                    <strong>UX Designer based in NYC—</strong>
+                  </h2>
+
+                  <h3>
+                    Currently at Vertic, creating intentional and thoughtful
+                    experiences for users.
+                  </h3>
+                  <h3>
+                    I'm always happy to chat about
+                    <br />
+                    <span className="topic" ref={this.topicRef}>
+                      User experience design
+                    </span>
+                  </h3>
+                </div>
+                <div className="section">
+                  <div className="background-color-block" />
+                  <section id="project1" className="section">
+                    <div className="project-container">
+                      <div className="project-title">
+                        <h1 className="big-title"> CSL</h1>
+                        <div className="project-short-description">
+                          <div className="dash"> </div>
+                          <p>
+                            {" "}
+                            Led the UX Design of the website redesign of CSL and
+                            CSL Behring, a global specialty biotechnology
+                            company.
+                          </p>
+                        </div>
+                      </div>
+
+                      <div className="project-img">
+                        <img src={poop} />
+                      </div>
+                    </div>
+                  </section>
+                </div>
+                <div className="section">
+                  <section id="project2" className="section">
+                    <div className="project-container">
+                      <div className="project-title">
+                        <h1 className="big-title"> SAP Ariba Brand Tool</h1>
+                        <div className="project-short-description">
+                          <div className="dash"> </div>
+                          <p>
+                            {" "}
+                            Designed a new brand central for users to access the
+                            SAP Ariba brand assets and images.
+                          </p>
+                        </div>
+                      </div>
+
+                      <div className="project-img">
+                        <img src={poop} />
+                      </div>
+                    </div>
+                  </section>
+                </div>
+                <div className="section">
+                  <section id="project3" className="section">
+                    <div className="project-container">
+                      <div className="project-title">
+                        <h1 className="big-title"> GDPR Benchmark</h1>
+                        <div className="project-short-description">
+                          <div className="dash"> </div>
+                          <p>
+                            {" "}
+                            Designed an assessment tool used to assess the
+                            readiness towards being GDPR compliant
+                          </p>
+                        </div>
+                      </div>
+
+                      <div className="project-img">
+                        <img src={poop} />
+                      </div>
+                    </div>
+                  </section>
+                </div>
+                <div className="section">
+                  <section id="project4" className="section">
+                    <div className="project-container">
+                      <div className="project-title">
+                        <h1 className="big-title"> Barter Sugar</h1>
+                        <div className="project-short-description">
+                          <div className="dash"> </div>
+                          <p>
+                            {" "}
+                            Strategy and design of a global marketplace
+                            experience where people can connect to trade goods,
+                            services, and spaces
+                          </p>
+                        </div>
+                      </div>
+
+                      <div className="project-img">
+                        <img src={poop} />
+                      </div>
+                    </div>
+                  </section>
+                </div>
+                <div className="section">
+                  <Link to="/projects/">Go to page 2</Link>
+                </div>
               </div>
-            </div>
-
-            <div class="project-img">
-              <img src={poop} />
-            </div>
-          </div>
-        </section>
-        <br />
-        <br />
-        <br />
-        <section id="project2" class="section">
-          <div class="project-container">
-            <div class="project-title">
-              <h1 class="big-title"> SAP Ariba Brand Tool</h1>
-              <div class="project-short-description">
-                <div class="dash"> </div>
-                <p>
-                  {" "}
-                  Designed a new brand central for users to access the SAP Ariba
-                  brand assets and images.
-                </p>
-              </div>
-            </div>
-
-            <div class="project-img">
-              <img src={poop} />
-            </div>
-          </div>
-        </section>
-        <br />
-        <br />
-        <section id="project3" class="section">
-          <div class="project-container">
-            <div class="project-title">
-              <h1 class="big-title"> GDPR Benchmark</h1>
-              <div class="project-short-description">
-                <div class="dash"> </div>
-                <p>
-                  {" "}
-                  Designed an assessment tool used to assess the readiness
-                  towards being GDPR compliant
-                </p>
-              </div>
-            </div>
-
-            <div class="project-img">
-              <img src={poop} />
-            </div>
-          </div>
-        </section>
-        <br />
-        <br />
-        <section id="project4" class="section">
-          <div class="project-container">
-            <div class="project-title">
-              <h1 class="big-title"> Barter Sugar</h1>
-              <div class="project-short-description">
-                <div class="dash"> </div>
-                <p>
-                  {" "}
-                  Strategy and design of a global marketplace experience where
-                  people can connect to trade goods, services, and spaces
-                </p>
-              </div>
-            </div>
-
-            <div class="project-img">
-              <img src={poop} />
-            </div>
-          </div>
-        </section>
-        <br />
-        <br />
-        <Link to="/projects/">Go to page 2</Link>
+            )
+          }}
+        />
       </Layout>
     )
   }
